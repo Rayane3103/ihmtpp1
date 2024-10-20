bf937b17488d8661fca031b33d832936ceb1e24c
Rayane3103 <rayanemoumine2405@gmail.com>
2024-10-20 10:53:03 +0100

    adding components

--- lib/page1.dart
+++ lib/page1.dart
@@ -5,6 +5,25 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Placeholder(),);
+    return Scaffold(body: Row(children: [
+      Container(
+        color:Colors.red,
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width*0.35,
+      ),
+      Container(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width*0.65,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Text('Personal Informations:')
+            ],
+          ),
+        ),
+      ),
+
+    ],),);
   }
 }
